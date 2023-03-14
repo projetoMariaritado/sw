59ad105ce6f87fd2bd03c0194306ff972c80d4e5
Maria Rita <100208753+projetoMariaritado@users.noreply.github.com>
2023-03-14 12:45:24 -0300

    Add files via upload

--- ExercicioAlunos/Program.cs
+++ ExercicioAlunos/Program.cs
@@ -1,14 +1,26 @@
-﻿namespace ExercicioAlunos;
+﻿
+
+namespace ExercicioAlunos;
 class Program
 {
     static void Main(string[] args)
     {
+
         Alunos aluno1 = new Alunos();
-        aluno1.nome = " Outro Aluno";
-        aluno1.nota1 = 4;
-        aluno1. nota2 = 5;
+        Console.WriteLine("Digite o nome do aluno: ");
+        aluno1.nome = Console.ReadLine(); 
+
+
+        Console.WriteLine("Digite a 1° nota: ");
+        aluno1.nota1 = double.Parse(Console.ReadLine());
+
+       
+        Console.WriteLine("Digite a 2° Nota: ");
+        aluno1.nota2 = double.Parse(Console.ReadLine());
+        
 
         aluno1.mensagem();
 
     }
+
 }
